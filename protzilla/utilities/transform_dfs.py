dc9fc74e317abea886b33d8e4191bbd5156ba079
AK <kuganash.ravishanker@student.hpi.de>
2024-07-10 11:32:13 +0200

    updated transform_dfs.py so that it supports peptide DFs

--- protzilla/utilities/transform_dfs.py
+++ protzilla/utilities/transform_dfs.py
@@ -17,10 +17,17 @@ def long_to_wide(intensity_df: pd.DataFrame, value_name: str = None):
         packages such as sklearn
     :rtype: pd.DataFrame
     """
+
+    if intensity_df.duplicated(subset=["Sample", "Protein ID"]).any():
+        intensity_df = intensity_df.groupby(["Sample", "Protein ID"]).mean().reset_index()
+        intensity_df = intensity_df.dropna()
+
     values_name = default_intensity_column(intensity_df) if value_name is None else value_name
-    return pd.pivot(
+    intensity_df = pd.pivot(
         intensity_df, index="Sample", columns="Protein ID", values=values_name
     )
+    intensity_df = intensity_df.fillna(intensity_df.mean())
+    return intensity_df
 
 
 def wide_to_long(wide_df: pd.DataFrame, original_long_df: pd.DataFrame):
@@ -40,26 +47,35 @@ def wide_to_long(wide_df: pd.DataFrame, original_long_df: pd.DataFrame):
     """
     # Read out info from original dataframe
     intensity_name = default_intensity_column(original_long_df)
-    gene_info = original_long_df["Gene"]
-    # Turn the wide format into the long format
-    intensity_df = pd.melt(
-        wide_df.reset_index(),
+
+    # Identify the additional columns from the original long dataframe
+    additional_columns = ['Modification', 'Retention Time']
+    existing_additional_columns = [col for col in additional_columns if col in original_long_df.columns]
+
+    # Melt the wide format back to long format
+    melted_df = pd.melt(
+        wide_df,
         id_vars="Sample",
         var_name="Protein ID",
         value_name=intensity_name,
     )
-    intensity_df.sort_values(
+    melted_df.sort_values(
         by=["Sample", "Protein ID"],
         ignore_index=True,
         inplace=True,
     )
-    intensity_df.insert(2, "Gene", gene_info)
 
-    return intensity_df
+    # Add back the additional columns if they exist in the original dataframe
+    for col in existing_additional_columns:
+        melted_df[col] = original_long_df[col]
+
+    return melted_df
 
 
 def is_long_format(df: pd.DataFrame):
-    return set(df.columns[:3]) == {"Sample", "Protein ID", "Gene"}
+    required_columns = {"Sample", "Protein ID"}
+    additional_columns = {"Gene", "Retention time"}
+    return required_columns.issubset(df.columns) and any(col in df.columns for col in additional_columns)
 
 
 def is_intensity_df(df: pd.DataFrame):
